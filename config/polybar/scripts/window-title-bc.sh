155bee3bcc85051cbb058e69014aad83d6282377
Idan <idang1410@gmail.com>
2020-06-28 19:40:51 +0300

    Improved window-title polybar script

--- config/polybar/scripts/window-title-bc.sh
+++ config/polybar/scripts/window-title-bc.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
-
-xprop -id $(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}') WM_CLASS | awk '{print $4}' | tr -d '"'
+if (( $(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}') )) ; then
+    xprop -id $(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}') WM_CLASS | awk '{print $4}' | tr -d '"';
+else
+    echo "None"
+fi
